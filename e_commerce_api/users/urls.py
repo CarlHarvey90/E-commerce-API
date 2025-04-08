8f30c551962f9c0bcfc8925820a4e532f6c9febe
Carl Harvey <c.harvey@hotmail.co.za>
2025-04-08 16:42:42 +0100

    Testing the ListCreateView from generic

--- e_commerce_api/users/urls.py
+++ e_commerce_api/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.users, name ='users'),
+    #path('users/', views.users, name ='users'),
+    path("users/", views.UsersListCreate.as_view(), name="users-view-create")
 ]
--- e_commerce_api/users/views.py
+++ e_commerce_api/users/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from rest_framework import generics
+from .models import Users
+from.serializers import UsersSerializer
+
+class UsersListCreate(generics.ListCreateAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
 
 def users(request):
     template = loader.get_template('base.html')
