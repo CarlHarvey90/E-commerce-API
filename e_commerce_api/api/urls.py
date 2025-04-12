31a531c90a47fc6ab0e644578e1835bf0ef405c6
Carl Harvey <49181506+CarlHarvey90@users.noreply.github.com>
2025-04-12 08:29:01 +0100

    Added final CRUD elements

--- e_commerce_api/api/urls.py
+++ e_commerce_api/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_user, create_user
+from .views import get_users, create_user, user_detail
 
 urlpatterns = [
     #path('users/', views.users, name ='users'),
-    path("get_user/", get_user, name="get_user"),
-    path("create_user/", create_user, name="create_user")
+    path("get_users/", get_users, name="get_users"),
+    path("create_user/", create_user, name="create_user"),
+    path("user/<int:pk>", user_detail, name="user_detail")
 ]
--- e_commerce_api/api/views.py
+++ e_commerce_api/api/views.py
@@ -6,7 +6,7 @@ from .models import Users
 from .serializer import UsersSerializer
 
 @api_view(['GET'])
-def get_user(request):
+def get_users(request):
     users = Users.objects.all()                                                 #Get all objects from user model
     serializer = UsersSerializer(users, many=True)                              #serialize the data
     return Response(serializer.data)                                            #return data
@@ -18,3 +18,25 @@ def create_user(request):
         serializer.save()                                                       #save the data
         return Response(serializer.data, status=status.HTTP_201_CREATED)        #return data and http response
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)      #return error and http response
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    try:
+        user = Users.objects.get(pk=pk)
+    except Users.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = UsersSerializer(user)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        serializer = UsersSerializer(user, data=request.data)                             
+        if serializer.is_valid():                                                   
+            serializer.save() 
+            return Response(serializer.data)        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    
+    if request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
