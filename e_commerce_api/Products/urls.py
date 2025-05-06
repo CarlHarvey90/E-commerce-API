8d1eef164c859efcdec54f1e4efdaece53aa58a3
Carl Harvey <49181506+CarlHarvey90@users.noreply.github.com>
2025-05-06 16:23:56 +0100

    Product Detail GET PUT DELETE

--- e_commerce_api/Products/urls.py
+++ e_commerce_api/Products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_products, create_products
+from .views import get_products, create_products, product_detail
 
 urlpatterns = [
     path("get_products/", get_products, name="get_products"),
     path("create_products/", create_products, name="create_products"),
+    path("product_detail/<int:pk>", product_detail, name="product_detail"),
 ]
